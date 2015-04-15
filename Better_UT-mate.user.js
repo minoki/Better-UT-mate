dc0774cc6b0bad8e61cfad350626d0c02968e127
ARATA Mizuki <minorinoki@gmail.com>
2015-04-15 15:39:27 +0900

    Version 2

--- Better_UT-mate.user.js
+++ Better_UT-mate.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name        Better UT-mate
 // @namespace   http://d-poppo.nazo.cc/
-// @include     https://ut-gakumu.adm.u-tokyo.ac.jp/websys/campus
-// @version     1
+// @include     https://ut-gakumu.adm.u-tokyo.ac.jp/websys/campus*
+// @version     2
+// @grant       none
 // ==/UserScript==
 
 window.addEventListener('load',function(){
@@ -11,12 +12,26 @@ window.addEventListener('load',function(){
     return r.snapshotItem(0);
   }
 
+  // 「ログイン」
   var a = getElementByXPath(".//a[@href='JavaScript:login();']")
-  if (!a) {
-    return;
+  if (a) {
+    var b = document.createElement("button");
+    b.setAttribute("type","submit");
+    b.style.display='none';
+    a.parentNode.insertBefore(b,a);
   }
-  var b = document.createElement("button");
-  b.setAttribute("type","submit");
-  b.style.display='none';
-  a.parentNode.insertBefore(b,a);
+
+  //// func=function.syllabus.refer
+
+  // 「授業科目決定」
+  var b = getElementByXPath(".//form[@name='InputForm']//input[@type='button' and @onclick='noInput()']");
+  if (b) {
+    b.type="submit";
+  }
+
+  // 「検索開始」
+  var b = getElementByXPath(".//form[@name='SearchForm']//input[@type='button' and @onclick=\"func.value='function.syllabus.refer.search';submit();\"]");
+  if (b) {
+    b.type="submit";
+  }
 },false);
