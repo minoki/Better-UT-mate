d04117bb6f1b04cc216ddc2b80bdad9cd44d4c9e
ARATA Mizuki <minorinoki@gmail.com>
2015-04-15 15:39:49 +0900

    Version 3

--- Better_UT-mate.user.js
+++ Better_UT-mate.user.js
@@ -2,7 +2,7 @@
 // @name        Better UT-mate
 // @namespace   http://d-poppo.nazo.cc/
 // @include     https://ut-gakumu.adm.u-tokyo.ac.jp/websys/campus*
-// @version     2
+// @version     3
 // @grant       none
 // ==/UserScript==
 
@@ -13,25 +13,66 @@ window.addEventListener('load',function(){
   }
 
   // 「ログイン」
-  var a = getElementByXPath(".//a[@href='JavaScript:login();']")
-  if (a) {
+  var e = getElementByXPath(".//a[@href='JavaScript:login();']")
+  if (e) {
     var b = document.createElement("button");
     b.setAttribute("type","submit");
     b.style.display='none';
-    a.parentNode.insertBefore(b,a);
+    e.parentNode.insertBefore(b,e);
   }
 
   //// func=function.syllabus.refer
 
   // 「授業科目決定」
-  var b = getElementByXPath(".//form[@name='InputForm']//input[@type='button' and @onclick='noInput()']");
-  if (b) {
-    b.type="submit";
+  var e = getElementByXPath(".//form[@name='InputForm']//input[@type='button' and @onclick='noInput()']");
+  if (e) {
+    e.type="submit";
   }
 
   // 「検索開始」
-  var b = getElementByXPath(".//form[@name='SearchForm']//input[@type='button' and @onclick=\"func.value='function.syllabus.refer.search';submit();\"]");
-  if (b) {
-    b.type="submit";
+  var e = getElementByXPath(".//form[@name='SearchForm']//input[@type='button' and @onclick=\"func.value='function.syllabus.refer.search';submit();\"]");
+  if (e) {
+    e.type="submit";
   }
+
+  //// func=function.seiseki.refer.span or func=function.seiseki.tani.span
+
+  // 「過去を含めた全成績」
+  var e = getElementByXPath(".//form[@name='InputForm']//input[@type='radio' and @name='out_range' and @value='1']");
+  if (e) {
+    var parent = e.parentNode;
+    var label = document.createElement("label");
+    var text = e.nextSibling; // 「過去を含めた全成績」
+    parent.insertBefore(label, e);
+    label.appendChild(parent.removeChild(e));
+    label.appendChild(parent.removeChild(text));
+  }
+
+  // 「対象年度・学期の成績のみ→」
+  var out_range_2 = getElementByXPath(".//form[@name='InputForm']//input[@type='radio' and @name='out_range' and @value='2']");
+  if (e) {
+    var parent = out_range_2.parentNode;
+    var label = document.createElement("label");
+    var text = out_range_2.nextSibling; // 「対象年度・学期の成績のみ→」
+    parent.insertBefore(label, out_range_2);
+    label.appendChild(parent.removeChild(out_range_2));
+    label.appendChild(parent.removeChild(text));
+
+    // 「年度」
+    var nendo = getElementByXPath(".//form[@name='InputForm']//input[@type='text' and @name='nendo']");
+    if (nendo) {
+      nendo.addEventListener("focus", function() {
+        out_range_2.checked = true;
+      }, false);
+    }
+
+    // 学期
+    var gakki = getElementByXPath(".//form[@name='InputForm']//select[@name='term']");
+    if (gakki) {
+      gakki.addEventListener("focus", function() {
+        out_range_2.checked = true;
+      }, false);
+    }
+  }
+
 },false);
